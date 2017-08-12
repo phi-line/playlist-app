8c20c5be7ae54c93f86c52baa2fdaece0afe924f
WasianWay <byron4white@gmail.com.com>
2017-08-12 14:51:33 -0700

    adding functionality for input and YT player

---
+++ scripts/q-input.js
@@ -0,0 +1,10 @@
+const searchInput = document.getElementById("searchInput");
+const searchSubmit = document.getElementById("searchSubmit");
+const player = document.getElementById("player");
+
+searchSubmit.addEventListener("click", qVideo);
+
+function qVideo(player, searchInput.value)
+{
+  
+}
