0d58efe714f6f63d14f390c7d4c8ee91bb694026
WasianWay <byron4white@gmail.com.com>
2017-08-12 16:55:13 -0700

    remove submit

--- scripts/q-input.js
+++ scripts/q-input.js
@@ -1,18 +1,19 @@
 const searchInput = document.getElementById('searchInput');
-const searchSubmit = document.getElementById('searchSubmit');
 const queue = [];
 
-searchSubmit.addEventListener('click', qVideo);
+searchInput.onkeypress = function(e){
+    if (e.keyCode == '13'){
+      qVideo();
+    }
+  }
 
 function qVideo(){
   let youtube = parseInput();
   if(!youtube) return;
-
   let videoObj = { id: youtube[1], h: youtube[3] || 0, m: youtube[4] || 0, s: youtube[5] || youtube[2] || 0 };
-
   queue.push(videoObj);
-
   console.log('video queued');
+  searchInput.value='';
 }
 
 function parseInput()
