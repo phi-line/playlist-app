74f93009ff8dc5599286efb91cd6c9d58739687b
WasianWay <byron4white@gmail.com.com>
2017-08-12 15:26:38 -0700

    added queuing funcitonality

--- scripts/q-input.js
+++ scripts/q-input.js
@@ -2,10 +2,14 @@ const searchInput = document.getElementById("searchInput");
 const searchSubmit = document.getElementById("searchSubmit");
 const player = document.getElementById("player");
 const query = searchInput.value;
+const youtube = /youtu(?:\.be\/|be\.com\/(?:embed\/|.*v=))([\w-]+)(?:.*start=(\d+)|.*t=(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?)?/.exec(query);
 
 searchSubmit.addEventListener("click", qVideo);
 
 function qVideo(player, query)
 {
-  
+  let videoObj = { id: youtube[1], h: youtube[3] || 0, m: youtube[4] || 0, s: youtube[5] || youtube[2] || 0 };
+
+
+  player.cueVideoById(videoObj.id, (videoObj.h * 3600 + videoObj.m * 60 + videoObj.s|0))
 }
