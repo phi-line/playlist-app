019cd733156ae56747c5500aa593c2f758cb0bd7
WasianWay <byron4white@gmail.com.com>
2017-08-12 15:33:32 -0700

    double to single quotations

--- scripts/q-input.js
+++ scripts/q-input.js
@@ -1,10 +1,10 @@
-const searchInput = document.getElementById("searchInput");
-const searchSubmit = document.getElementById("searchSubmit");
-const player = document.getElementById("player");
+const searchInput = document.getElementById('searchInput');
+const searchSubmit = document.getElementById('searchSubmit');
+const player = document.getElementById('player');
 const query = searchInput.value;
 const youtube = /youtu(?:\.be\/|be\.com\/(?:embed\/|.*v=))([\w-]+)(?:.*start=(\d+)|.*t=(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?)?/.exec(query);
 
-searchSubmit.addEventListener("click", qVideo);
+searchSubmit.addEventListener('click', qVideo);
 
 function qVideo(player, query)
 {
@@ -12,5 +12,5 @@ function qVideo(player, query)
 
   player.cueVideoById(videoObj.id, (videoObj.h * 3600 + videoObj.m * 60 + videoObj.s|0));
 
-  
+
 }
