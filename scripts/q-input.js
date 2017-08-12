0a6950c19658c49282af8b89b67f95fc8d629c12
WasianWay <byron4white@gmail.com.com>
2017-08-12 16:15:12 -0700

    queueing somewhat functional

--- scripts/q-input.js
+++ scripts/q-input.js
@@ -1,16 +1,20 @@
 const searchInput = document.getElementById('searchInput');
 const searchSubmit = document.getElementById('searchSubmit');
-const player = document.getElementById('player');
-const query = searchInput.value;
-const youtube = /youtu(?:\.be\/|be\.com\/(?:embed\/|.*v=))([\w-]+)(?:.*start=(\d+)|.*t=(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?)?/.exec(query);
 
 searchSubmit.addEventListener('click', qVideo);
 
-function qVideo(player, query)
-{
+function qVideo(){
+  let youtube = parseInput();
+  if(!youtube) return;
+
   let videoObj = { id: youtube[1], h: youtube[3] || 0, m: youtube[4] || 0, s: youtube[5] || youtube[2] || 0 };
 
   player.cueVideoById(videoObj.id, (videoObj.h * 3600 + videoObj.m * 60 + videoObj.s|0));
 
+  console.log('video queued');
+}
 
+function parseInput()
+{
+  return /youtu(?:\.be\/|be\.com\/(?:embed\/|.*v=))([\w-]+)(?:.*start=(\d+)|.*t=(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s)?)?/.exec(searchInput.value);
 }
